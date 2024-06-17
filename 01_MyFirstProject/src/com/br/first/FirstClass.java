e6ded57d03f2d62f00cde98da9f5988a367be7c4
Kimdonggyu26 <anima9125@gmail.com>
2024-06-17 17:38:08 +0900

    첫 프로젝트 생성
    FirstClass 만들기

---
+++ 01_MyFirstProject/src/com/br/first/FirstClass.java
@@ -0,0 +1,27 @@
+package com.br.first;
+
+public class FirstClass {
+
+	// single-line comment(주석) : 소스코드와 무관한 내용을 기술 / 코드로써 인식되지 않음
+	
+	/*
+	 * multi-line comment
+	 * 여러줄 주석
+	 * 
+	 * 1. 프로젝트 만들기
+	 * 2. 패키지 만들기
+	 * 3. 클래스 만들기
+	 * 4. 클래스 내에 소스코드 작성
+	 * 
+	 * 
+	*/
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(2);
+	}
+	
+	
+}
